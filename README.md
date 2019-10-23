24960417d3add4dec8be4712e585fc760be946e9
kerttujaansalu <kerttu.jaansalu@khk.ee>
2019-10-23 16:18:04 +0300

    Andmetüübid

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 * github repo loomine - pa
 * VSCode remote seadistamine
 * Kursuse põhifailide loomine ja testimine
+## Materjalid
+* PA käsiraamat [https://web.htk.tlu.ee/digitaru/programmeerimine/part/sissejuhatus/]
--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('JS test');
+// Andmetüübid
+
+// täisarvud
+console.log(10);
+console.log(-4);
+console.log(0);
+
+// reaakarvyd - ujuvkomaarvud
+console.log(2.5);
+console.log(-7.23);
+console.log(-2.0);
+
+// sõned - string - tekstid
+console.log('Hello World!');
+
+// tõeväärtused
+console.log(true); // tõene
+console.log(false); // väär
+
+// nimekirjad - list
+console.log([1, 2, 3, 4]);
+
+// objektid
+console.log({a : 1, b : 2});
+console.table({a : 1, b : 2});
