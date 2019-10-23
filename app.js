d4f3c0a7987a2768147d6b63055149ea360a8b57
kerttujaansalu <kerttu.jaansalu@khk.ee>
2019-10-23 16:24:54 +0300

    konsooli kasutamisvõimalused info väljastamiseks

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// Andmetüübid
+// Logid konsoolile
 
-// täisarvud
-console.log(10);
-console.log(-4);
-console.log(0);
+// logid
+console.log('123');
 
-// reaakarvyd - ujuvkomaarvud
-console.log(2.5);
-console.log(-7.23);
-console.log(-2.0);
-
-// sõned - string - tekstid
-console.log('Hello World!');
-
-// tõeväärtused
-console.log(true); // tõene
-console.log(false); // väär
-
-// nimekirjad - list
-console.log([1, 2, 3, 4]);
-
-// objektid
-console.log({a : 1, b : 2});
+// tabel 
 console.table({a : 1, b : 2});
+
+// veateade 
+console.error('See on mingi viga');
+
+// märkused
+console.warn('See on märkus');
+
+//ajakulu kontroll
+console.time('Test');
+    console.log('Mingi tegevus');
+    console.log('Mingi tegevus');
+    console.log('Mingi tegevus');
+    console.log('Mingi tegevus');
+    console.log('Mingi tegevus');
+console.timeEnd('Test');
+
+ // kustutamine - ekraani puhastamine
+console.clear();
