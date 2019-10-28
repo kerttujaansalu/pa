6044f621d5231ce58d7cdc6b652b6f7251e04294
kerttujaansalu <kerttu.jaansalu@khk.ee>
2019-10-28 11:49:07 +0200

    algarvu kontrollimine

--- app.js
+++ app.js
@@ -2,10 +2,18 @@
 
 // while
 //eelkontrolliga tsükkel
-let number = 1; // defineerime tsükli juhtimismuutuja
-// paneme kontrolltingimuse paika
-while(number <= 10) {
-    console.log(number);
-    // suurendame juhtimismuutuja väärtus
-    number++;
+// defineerime numbri
+let arv = 5; 
+// defineerime jagaja algväärtuse
+let jagaja = 2;
+// suurendame jagaja väärtust niikaua kui
+// jagamise jääk on 0
+while(arv % jagaja != 0){
+    jagaja++;
+}
+// kontrollime kas jagasime sama arvuga
+if(arv == jagaja) {
+    console.log(arv + ' on algarv');
+} else {
+    console.log(arv + ' ei ole algarv');
 }
