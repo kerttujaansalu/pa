be8029a1138d4a738292f333f1f83f718bdc5bbd
kerttujaansalu <kerttu.jaansalu@khk.ee>
2019-11-11 10:26:40 +0200

    muutujate scope

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-// tsüklid
+// scope - nähtavus
 
-// while
-//eelkontrolliga tsükkel
-// defineerime numbri
-let arv = 5; 
-// defineerime jagaja algväärtuse
-let jagaja = 2;
-// suurendame jagaja väärtust niikaua kui
-// jagamise jääk on 0
-while(arv % jagaja != 0){
-    jagaja++;
-}
-// kontrollime kas jagasime sama arvuga
-if(arv == jagaja) {
-    console.log(arv + ' on algarv');
-} else {
-    console.log(arv + ' ei ole algarv');
-}
+//globaalne muutujate defineerimine
+var a = 1;
+let b = 2;
+const c = 3;
+
+console.log('Globaalne: ', a, b, c);
