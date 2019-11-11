7ee1c7e6a42195f52b0ecfc65797e2d3d4d94247
kerttujaansalu <kerttu.jaansalu@khk.ee>
2019-11-11 10:30:40 +0200

    muutuja scope plokk

--- app.js
+++ app.js
@@ -5,4 +5,16 @@ var a = 1;
 let b = 2;
 const c = 3;
 
+// plokk
+if(true) {
+    var a = 4;
+    let b = 5;
+    const c = 6;
+    console.log('Plokk: ', a, b, c);
+}
+
+for(var a = 0; a < 10; a++); {
+    console.log('Tsükkel: ', a);
+}
 console.log('Globaalne: ', a, b, c);
+
